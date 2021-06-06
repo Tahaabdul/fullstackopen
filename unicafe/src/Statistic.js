90ec80e123ab6e325c39d23ce3e02cf2dc3345b9
Tahaabdul <rhaxaqi@gmail.com>
2021-06-06 17:02:50 +0000

    fixed DOM error

--- unicafe/src/Statistic.js
+++ unicafe/src/Statistic.js
@@ -1,10 +1,9 @@
 const Statistic = ({ text, value }) => {
   return (
-    <div>
-      <tr>
-        <td>{text}</td> <td>{value}</td>
-      </tr>
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
